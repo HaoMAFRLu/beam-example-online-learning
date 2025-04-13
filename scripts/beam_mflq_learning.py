27e9271d583c962ccf79d284c4f5e64794088746
HaoMAFRLu <hao.ma@tuebingen.mpg.de>
2025-04-13 13:08:00 +0200

    v2

--- scripts/beam_mflq_learning.py
+++ scripts/beam_mflq_learning.py
@@ -30,9 +30,9 @@ def main(mode,
     learning.learning()
 
 if __name__ == '__main__':
-    main(mode='v1',
+    main(mode='v2',
          T=300,
          xi=0.00001,
-         exp_name='mlfqv1',
+         exp_name='mlfqv2',
          _w=0.3,
          _sigma_a=1.0)
